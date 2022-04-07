20e8579cc4e7b72c38622f7baec54b6c6fa6b6bd
devbyul <kinsng1599@gmail.com>
2022-04-07 18:45:43 +0900

    feat: MetaMask Template init

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
+/* eslint-disable no-console */
+import { useWeb3React } from '@web3-react/core';
+import React, { useState } from 'react';
+// import Web3 from 'web3';
 import { Container } from './styles';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare let window: (Window & typeof globalThis) | any;
 
 // eslint-disable-next-line react/function-component-definition
 const Page: React.FC = () => {
+  const { account, chainId, active } = useWeb3React();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [targetAccount, setTargetAccount] = useState<string>(
+    `${process.env.REACT_APP_TARGETADDRESS}`
+  );
+
+  const [balance, setBalacne] = useState<number>(0);
+  const [symbol, setSymbol] = useState<string>(' -');
+  const [sendTokenBalance, setSendTokenBalance] = useState<number>(0);
+
+  const targetAccountChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setTargetAccount(event.target.value);
+  };
+  const sendTokenBalanceChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setSendTokenBalance(Number(event.target.value));
+  };
+
+  const activateWallet = async () => {
+    console.log('[activateWallet]=======================================');
+  };
+  const contractTTK = async () => {
+    console.log('[contractTTK]=======================================');
+  };
+  const accountBalance = async () => {
+    console.log('[accountBalance]=======================================');
+  };
+
+  const handleSendTransaction = async () => {
+    console.log(
+      '[handleSendTransaction]======================================='
+    );
+  };
+
   return (
     <Container>
       <div>
         <h1 className="center">지갑</h1>
-        <input type="text" name="account" id="account" value="0000000000" disabled />
-        <button type="submit">지갑 가져오기</button>
+        <p>Account : {account}</p>
+        <p>ChainId : {chainId}</p>
+        <p>active : {active}</p>
+        <button type="submit" onClick={activateWallet}>
+          지갑 가져오기
+        </button>
+        <button type="submit" onClick={contractTTK}>
+          컨트렉트 연결하기
+        </button>
       </div>
       <div>
         <h1 className="center">잔액</h1>
-        <input type="text" name="balance" id="balance" value="0000000000" disabled />
-        <button type="submit">잔액 새로고침</button>
+        <p>
+          balance : {balance}
+          {symbol}
+          {}
+        </p>
+        <button type="submit" onClick={accountBalance}>
+          잔액 새로고침
+        </button>
       </div>
       <div>
         <h1 className="center">보내기</h1>
-        <input type="text" name="target-account" id="target-account" value="0x40F12096f1aD66713C2fF56a04E4E3245DF775EA" disabled />
-        <button type="submit">보내기</button>
+        <input
+          type="text"
+          name="target-account"
+          id="target-account"
+          value={targetAccount}
+          onChange={targetAccountChange}
+        />
+        <input
+          type="text"
+          name="target-account"
+          id="target-account"
+          value={sendTokenBalance}
+          onChange={sendTokenBalanceChange}
+        />
+        <button type="submit" onClick={handleSendTransaction}>
+          보내기
+        </button>
       </div>
     </Container>
   );
