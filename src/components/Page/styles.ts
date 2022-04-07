e7f8edee9db6a984ba10d8e040b0dac4299ee528
devbyul <kinsng1599@gmail.com>
2022-04-07 18:32:23 +0900

    art: btn-center, input width, (buttn,p) margin

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -2,14 +2,20 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: #dfdfdf;
-  height: 500px;
+  height: 600px;
   .center {
     text-align: center;
+  }
+  .btn-center {
+    display: block;
+    margin: 0 auto;
   }
   div {
     padding-top: 20px;
   }
   input {
+    display: block;
+    width: 450px;
     height: 40px;
     line-height: 40px;
     font-size: 18px;
@@ -18,5 +24,9 @@ export const Container = styled.div`
   button {
     height: 35px;
     padding: 5px;
+    margin: 10px 20px;
+  }
+  p {
+    margin-left: 15px;
   }
 `;
