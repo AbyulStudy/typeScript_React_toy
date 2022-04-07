e8e491618ff56d36f2e46f1f3b8ed82a29368a8e
devbyul <kinsng1599@gmail.com>
2022-04-07 10:35:19 +0900

    feat: styled-components init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>지갑</div>
+    </div>
+  );
 }
 
 export default App;
